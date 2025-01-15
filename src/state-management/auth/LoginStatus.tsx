94a843d5816a8cf3f278d79f824ff70012d84742
mahmoudmaged226 <mahmoudmaged226@gmail.com>
2025-01-15 22:49:44 +0200

    Implementing zustand in auth module

--- src/state-management/auth/LoginStatus.tsx
+++ src/state-management/auth/LoginStatus.tsx
@@ -1,13 +1,14 @@
-import useAuth from "./useAuth";
+import useAuthStore from "./store";
+
 
 const LoginStatus = () => {
-  const { user, dispatch } = useAuth();
+  const {user, login, logout} = useAuthStore();
   if (user)
     return (
       <>
         <div>
           <span className="mx-2">{user}</span>
-          <a onClick={() => dispatch({ type: "LOGOUT" })} href="#">
+          <a onClick={() => logout()} href="#">
             Logout
           </a>
         </div>
@@ -16,7 +17,7 @@ const LoginStatus = () => {
   return (
     <div>
       <a
-        onClick={() => dispatch({ type: "LOGIN", username: "mahmoud.maged" })}
+        onClick={() => login("mahmoud.maged")}
         href="#"
       >
         Login
--- src/state-management/tasks/TaskList.tsx
+++ src/state-management/tasks/TaskList.tsx
@@ -1,12 +1,13 @@
 import { useContext } from "react";
 import TasksContext from "./tasksContext";
-import useAuth from "../auth/useAuth";
+import useAuthStore from "../auth/store";
+
 
 const useTasks = () => useContext(TasksContext);
 
 const TaskList = () => {
   const { tasks, dispatch } = useTasks();
-  const { user } = useAuth();
+  const { user } = useAuthStore();
   return (
     <>
       <p>User: {user}</p>
