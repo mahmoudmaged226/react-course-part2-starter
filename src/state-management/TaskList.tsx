d2712c98376e5d2c61fb5c45fe36fbbf667cf977
mahmoudmaged226 <mahmoudmaged226@gmail.com>
2025-01-15 20:55:54 +0200

    Sharing context between user and tasklist

--- src/state-management/TaskList.tsx
+++ src/state-management/TaskList.tsx
@@ -1,11 +1,14 @@
 import { useContext, useReducer, useState } from "react";
 import tasksReducer from "./reducers/tasksReducer";
 import TasksContext from "./contexts/tasksContext";
+import AuthContext from "./contexts/authContext";
 
 const TaskList = () => {
   const { tasks, dispatch } = useContext(TasksContext);
+  const { user } = useContext(AuthContext);
   return (
     <>
+      <p>User: {user}</p>
       <button
         onClick={() =>
           dispatch({
